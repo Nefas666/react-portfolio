92b1b05972298336e02c08e2a952b5ae3621b1cf
Selene Manno <s.manno@conversion-e3.com>
2022-01-16 22:32:09 +0100

    feat: (Header.js) :art: Add component

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,55 @@
 import * as React from "react"
-import PropTypes from "prop-types"
+//import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import logo from "../images/game.gif"
+import "./Header.css"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+class Header extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      hasScrolled:false
+    }
+  }
+  componentDidMount (){
+    window.addEventListener('scroll',
+    this.handleScroll)
+  }
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({hasScrolled:true})
+    } else {
+      this.setState ({hasScrolled:false })
+    }
+  }
+  render(){
+    return(
+      <div className={this.state.hasScrolled ? 'Header HeaderHasScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/">
+            <img src={logo} width="60" />
+          </Link>
+          <Link to="/projects"> Projects </Link> 
+          <Link to="/bio"> Bio </Link>
+          <Link to="/contacts"> Contacts </Link>
+        </div>
+      </div>  
+    )
+  }
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
+
+{
+  /*Header.propTypes = {
+        siteTitle: PropTypes.string,
+      }
+
+      Header.defaultProps = {
+        siteTitle: ``,
+      }*/
 }
 
 export default Header
