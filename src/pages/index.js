52456b81c4a64b92dab14135239f90fbae3af04b
Selene Manno <s.manno@conversion-e3.com>
2022-01-16 22:33:31 +0100

    chore: (/pages)  :construction:  Add component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,27 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+//import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
+//import Seo from "../components/seo"
+import Card from "../components/Card"
+
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1> Hi Digital World!</h1> 
-    <p> Welcome to my portfolio </p>
+    <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1> Hello Digital World!</h1>
+        <p> Welcome to my portfolio </p>
+        {/*<Link to="/page-2/"> See what I do here </Link> <br />*/}
+        <svg width="100%" height="172" viewBox="0 0 100% 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z" fill="#a378d9">
+          <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s" values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;  M0 86.3149C316 86.315 444 159.155 884 51.1554C1324 -56.8446 1320.29 34.1214 1538 70.4063C1814 116.407 2156 188.408 2560 86.315V232.317L0 232.316V86.3149Z;     M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;" />
+          </path> 
+        </svg>
+      </div>
+    </div>
     {/*<StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -18,9 +30,28 @@ const IndexPage = () => (
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />*/}
-    <p>
-      <Link to="/page-2/"> Go to page 2 </Link> <br />
-    </p>
+    <div className="Cards">
+      <h2>Form Latest to Oldest</h2>
+      <div className="CardGroup">
+      <Card
+      title="Project Title"
+      description="brief but exhaustive description"
+      />
+      <Card
+      title="Project Title"
+      description="brief but exhaustive description"
+      />
+      <Card
+      title="Project Title"
+      description="brief but exhaustive description"
+      />
+      <Card
+      title="Project Title"
+      description="brief but exhaustive description"
+      />
+      </div>
+    </div>
+  </div>
   </Layout>
 )
 
